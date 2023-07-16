ad68bb75ee782c84bf558d51eda9253d01bfae8b
NirojT <tmgnirah@gmail.com>
2023-07-16 10:50:59 +0545

    first commit

---
+++ src/test/java/Kanchanjunga/SpringSecurityDurgesh1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Kanchanjunga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDurgesh1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
