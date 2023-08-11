8e80a1319b8cfdf3f6c2bd8659b02cd7495c7787
A18yan <ma18yan@gmail.com>
2023-08-11 15:47:08 +0545

    commit

--- src/main/java/Kanchanjunga/Reposioteries/FoodStockRepo.java
+++ src/main/java/Kanchanjunga/Reposioteries/FoodStockRepo.java
@@ -9,17 +9,15 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 import Kanchanjunga.Entity.FoodStock;
-import Kanchanjunga.Entity.Payment;
 
-public interface FoodStockRepo extends MongoRepository<FoodStock, UUID>{
-	
-	 @Query("{ 'createdDate' : { $gte: ?0, $lte: ?1 } }")
-	    List<FoodStock> findExpenseBy1Day(LocalDateTime start, LocalDateTime end);
-	 
-	 @Query("{ 'createdDate' : { $gte: ?0, $lte: ?1 } }")
-	 List<FoodStock> findExpenseBy1Week(LocalDateTime start, LocalDateTime end);
+public interface FoodStockRepo extends MongoRepository<FoodStock, UUID> {
 
-	
-	 @Query("{ 'createdDate' : { $gte: ?0, $lte: ?1 } }")
-	    List<FoodStock> findExpenseByCurrentMonth(LocalDate startOfMonth, LocalDate startOfNextMonth);
+	@Query("{ 'createdDate' : { $gte: ?0, $lte: ?1 } }")
+	List<FoodStock> findExpenseBy1Day(LocalDateTime start, LocalDateTime end);
+
+	@Query("{ 'createdDate' : { $gte: ?0, $lte: ?1 } }")
+	List<FoodStock> findExpenseBy1Week(LocalDateTime start, LocalDateTime end);
+
+	@Query("{ 'createdDate' : { $gte: ?0, $lte: ?1 } }")
+	List<FoodStock> findExpenseByCurrentMonth(LocalDate startOfMonth, LocalDate startOfNextMonth);
 }
